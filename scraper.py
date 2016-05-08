d1fb5be6a3780e7d0ea892c866711ef8eec7f4b4
Alex Morega <alex@grep.ro>
2016-05-08 16:53:05 +0300

    enumerate hospitals

---
+++ scraper.py
@@ -0,0 +1,32 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+def all_text(node):
+    return ' '.join(node.css('::text').extract()).strip()
+
+class CheltuieliSpider(scrapy.Spider):
+    name = 'cheltuieli'
+
+    def start_requests(self):
+        yield scrapy.FormRequest(
+            'http://www.monitorizarecheltuieli.ms.ro/centralizator',
+            formdata={
+                'page': '1',
+                'cautare_text': '',
+                'data_filtrare': '2016-01',
+            },
+            callback=self.home,
+        )
+
+    def home(self, resp):
+        for tr in resp.css('.records tr')[1:]:
+            hospital = all_text(tr.css('td')[0])
+            [href] = tr.css('td')[1].css('a::attr(href)').extract()
+            print hospital, href
+
+process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+})
+
+process.crawl(CheltuieliSpider)
+process.start()
